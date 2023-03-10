60f876f639f69cc140c25ea59219354543ed0b9b
rafikicodez <105193513+rafikicodez@users.noreply.github.com>
2023-03-10 18:24:57 -0500

    Delete Program - SE7 - Nicholas DiCicco/Herzing-SE7-main directory

--- Program - SE7 - Nicholas DiCicco/Herzing-SE7-main/module-info.java
+++
@@ -1,8 +0,0 @@
-module ordermanagementsystem {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires java.sql;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml, javafx.base;
-}
